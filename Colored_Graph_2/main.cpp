ea232240fa647b351b4a0b792869d130a45db1c3
Egor Sipachev <egor@payzeal.co>
2022-05-09 16:10:41 +0300

    commit 1

--- Colored_Graph_2/main.cpp
+++ Colored_Graph_2/main.cpp
@@ -4,9 +4,25 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+namespace fs = filesystem;
+
+vector<vector<int>> read_graph(const string& name_file, vector<pair<int, int>>& list_edges, vector<int>& edges_counter) {
+    ifstream fin;
+    fin.open(name_file);
+    vector<vector<int>> graph;
+    int vertex, edges;
+    vector<pair<int, int>> pairs;
+    list_edges.clear();
+    edges_counter.clear();
+}
+
+int main() {
+
+    
 }
