e6bec90637ad54c23d5a8ff4ada1000cae2155f4
amdpayzeal <andrei@payzeal.co>
2022-05-09 16:02:19 +0300

    Initial Commit

---
+++ Colored_Graph_2/main.cpp
@@ -0,0 +1,12 @@
+//
+//  main.cpp
+//  Colored_Graph_2
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
