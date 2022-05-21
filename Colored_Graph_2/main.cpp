32affcac1b3c8a4aeed2729ca90a51a4f1975ac3
Egor Sipachev <egor@payzeal.co>
2022-05-21 16:48:41 +0300

    commit 6

--- Colored_Graph_2/main.cpp
+++ Colored_Graph_2/main.cpp
@@ -12,14 +12,40 @@
 using namespace std;
 namespace fs = filesystem;
 
-vector<vector<int>> read_graph(const string& name_file, vector<pair<int, int>>& list_edges, vector<int>& edges_counter) {
+vector<vector<int>> graph_obxod(const string& filename, vector<pair<int, int>>& edges_matrix, vector<int>& edges_schet) {
     ifstream fin;
-    fin.open(name_file);
-    vector<vector<int>> graph;
-    int vertex, edges;
+    fin.open(filename);
+    vector<vector<int>> graph_indexes;
+    int indexes, edges_num;
     vector<pair<int, int>> pairs;
-    list_edges.clear();
-    edges_counter.clear();
+    edges_matrix.clear();
+    edges_schet.clear();
+    if (fin.is_open()) {
+        fin >> indexes >> edges_num;
+        vector <int> v(indexes, 0);
+        graph_indexes.resize(indexes, v);
+        auto zero = make_pair(0, 0);
+        pairs.resize(graph_indexes.size(), zero);
+        for (int k = 0; k < edges_num; k++) {
+            int start, end;
+            fin >> start >> end;
+            auto p1 = make_pair(start, end);
+            edges_matrix.push_back(p1);
+            graph_indexes[start][end] = 1;
+            graph_indexes[end][start] = 1;
+            pairs[start].first += 1;
+            pairs[end].first += 1;
+            pairs[start].second = start;
+            pairs[end].second = end;
+        }
+    } else {
+        cout << "File do not exist!" << endl;
+    }
+    sort(pairs.begin(), pairs.end());
+    for(long int i = pairs.size()-1;i >= 0; i--){
+        edges_schet.push_back(pairs[i].second);
+    }
+    return graph_indexes;
 }
 
 int main() {
