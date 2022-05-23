3a93f26fecbfbaefd4b43611a9fafa1f01a9df86
Egor Sipachev <egor@payzeal.co>
2022-05-23 17:47:15 +0300

    commit last

--- Colored_Graph_2/main.cpp
+++ Colored_Graph_2/main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 namespace fs = filesystem;
 
-vector<vector<int>> graph_obxod(const string& filename, vector<pair<int, int>>& edges_matrix, vector<int>& edges_schet) {
+vector<vector<int>> obxod_grapha(const string& filename, vector<pair<int, int>>& edges_matrix, vector<int>& edges_schet) {
     ifstream fin;
     fin.open(filename);
     vector<vector<int>> graph_indexes;
@@ -48,7 +48,59 @@ vector<vector<int>> graph_obxod(const string& filename, vector<pair<int, int>>& 
     return graph_indexes;
 }
 
-int main() {
+vector<int> painting(vector<vector<int>>& graph, const vector<int>& edges_schet) {
+    vector<int> vecOfColors(graph.size(), -1);
+    for(auto it : edges_schet) { //Запускаю автоитератор по ребрам
+        int i = it;
+        vector<bool> iscolored(graph.size(), false);
+        for (int j = 0; j < graph.size(); j++) {
+            if (graph[i][j] == 1) {
+                if(vecOfColors[j] > -1){
+                    iscolored[vecOfColors[j]] = true;
+                }
+            }
+        }
+        for (int k = 0; k < graph.size(); k++) {
+            if (!iscolored[k]) {
+                vecOfColors[i] = k;
+                break;
+            }
+        }
+    }
+    return vecOfColors;
+}
 
+int main () {
+    char path[] = "/Users/egorsipachev/МГУУЧЕБА/программирование/semestr4/colored_graph/tests";
+    auto it = fs::directory_iterator(path);
+    vector<fs::path> array_path;
+    copy_if(fs::begin(it), fs::end(it), std::back_inserter(array_path),
+        [](const auto& entry) {
+            return fs::is_regular_file(entry);
+    });
     
+    
+    for (auto & j : array_path) {
+        vector<int> edges_mass;
+        vector<pair<int, int>> edges_pairs;
+        vector<vector<int>> graph = obxod_grapha(j.string(), edges_pairs, edges_mass);
+        vector<int> Graph_colored = painting(graph, edges_mass);
+        int max = -1;
+        for (int i = 0; i < Graph_colored.size(); i++) {
+            if (Graph_colored[i] > max) {
+                max = Graph_colored[i];
+            }
+        }
+        cout << j.string() << " " << max + 1 << endl;
+       
+        cout << endl;
+        for (auto i : edges_pairs) {
+            if (Graph_colored[i.first] == Graph_colored[i.second]) {
+                cout << "Error! Try test: " << j.string() << endl;
+                cout << i.first << " " << i.second << endl;
+                break;
+            }
+        }
+    }
+    return 0;
 }
